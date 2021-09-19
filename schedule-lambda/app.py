0287a00b895ebdccbfc838c5323f80ca5ed6d73b
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-09-19 17:19:50 +0900

    feat: Test ec2 deploy code

--- schedule-lambda/app.py
+++ schedule-lambda/app.py
@@ -1,29 +1,15 @@
-from chalice import Chalice
+import logging
+
+import boto3
+from chalice import Chalice, Rate
 
 app = Chalice(app_name='schedule-lambda')
+app.log.setLevel(logging.INFO)
+
+ec2 = boto3.client('ec2')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.schedule(Rate(1, unit=Rate.MINUTES))
+def regions(event):
+    response = ec2.describe_regions()
+    app.log.info(response['Regions'])
